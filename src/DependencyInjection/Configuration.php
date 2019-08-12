2cd46b17d9349d96c881101687fd184cc8ceed8b
Albert Casademont <albertcasademont@gmail.com>
2019-08-12 19:09:31 +0200

    Remove Symfony 4.2 deprecation notice (#25)
    * Remove Symfony 4.2 deprecation notice
    
    * Bad reference

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,8 +18,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('expert_coder_swiftmailer_send_grid');
+        $treeBuilder = new TreeBuilder('expert_coder_swiftmailer_send_grid');
+        
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('expert_coder_swiftmailer_send_grid');
+        }
 
         $children = $rootNode->isRequired()->fixXmlConfig('category')->children();
         $this->configureApiKey($children);
